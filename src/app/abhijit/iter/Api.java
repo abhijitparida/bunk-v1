b42f5a3c5967e6b6080775066e212f73c071c7f1
Abhijit <abhijitparida.me@gmail.com>
2016-01-22 07:25:08 +0530

    Bump to v1.0.0

--- src/app/abhijit/iter/Api.java
+++ src/app/abhijit/iter/Api.java
@@ -86,7 +86,7 @@ public class Api {
 
   private static String getCurrentVersion() {
     // implement this
-    return "0.1.0";
+    return "1.0.0";
   }
 
   private static boolean checkUpdate(String studentRollNumber) {
