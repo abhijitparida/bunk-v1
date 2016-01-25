ff21fd4949a3128fae1eb0bc2111cc98c7f18687
Abhijit <abhijitparida.me@gmail.com>
2016-01-25 15:11:16 +0530

    Bump to v1.4.1

--- src/app/abhijit/iter/Api.java
+++ src/app/abhijit/iter/Api.java
@@ -96,7 +96,7 @@ public class Api {
 
   private String getCurrentVersion() {
     // implement this
-    return "1.4.0";
+    return "1.4.1";
   }
 
   private boolean checkUpdate(String studentRollNumber) {
