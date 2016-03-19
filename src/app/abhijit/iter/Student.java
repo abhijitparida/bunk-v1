6adb8348afef7600eecc97eafe3629b2b3614b67
Abhijit <abhijitparida.me@gmail.com>
2016-03-19 18:53:39 +0530

    Refactor Student class

--- src/app/abhijit/iter/Student.java
+++ src/app/abhijit/iter/Student.java
@@ -1,46 +1,38 @@
 package app.abhijit.iter;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.net.URLDecoder;
-
 public class Student {
 
-  public String studentRollNumber;
-  public String instituteId;
-  public String studentId;
-  public String registrationId;
-  public String lastRefreshed;
-  public String sectionCode;
-  public String name;
-  public String programCode;
-  public String academicYear;
-  public Course[] attendance;
+  private String sectioncode;
+  private String name;
+  private String programcode;
+  private String academicyear;
+  private String enrollmentno;
+  private Course[] attendance;
 
-  public static Student parseJson(String json) {
-    Student student;
-    try {
-      JSONParser parser = new JSONParser();
-      JSONObject studentJson = (JSONObject) parser.parse(json);
-      JSONObject studentDetailsJson = (JSONObject) ((JSONArray) parser.parse(URLDecoder.decode(String.valueOf(studentJson.get("studentDetailsJson")), "UTF-8"))).get(0);
-      String attendanceJson = URLDecoder.decode(String.valueOf(studentJson.get("attendanceJson")), "UTF-8");
-      student = new Student();
-      student.studentRollNumber = String.valueOf(studentJson.get("studentRollNumber"));
-      student.instituteId = String.valueOf(studentJson.get("instituteId"));
-      student.studentId = String.valueOf(studentJson.get("studentId"));
-      student.registrationId = String.valueOf(studentJson.get("registrationId"));
-      student.lastRefreshed = String.valueOf(studentJson.get("lastRefreshed"));
-      student.sectionCode = String.valueOf(studentDetailsJson.get("sectioncode"));
-      student.name = String.valueOf(studentDetailsJson.get("name"));
-      student.programCode = String.valueOf(studentDetailsJson.get("programcode"));
-      student.academicYear = String.valueOf(studentDetailsJson.get("academicyear"));
-      student.attendance = Course.parseJson(attendanceJson);
-    } catch (Exception e) {
-      student = null;
-    }
-    return student;
+  // TODO: Add a constructor
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getRollNumber() {
+    return this.enrollmentno;
+  }
+
+  public String getSectionCode() {
+    return this.sectioncode;
+  }
+
+  public String getProgramCode() {
+    return this.programcode;
+  }
+
+  public String getAcademicYear() {
+    return this.academicyear;
+  }
+
+  public Course[] getAttendance() {
+    return this.attendance;
   }
 
 }
