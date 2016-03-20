78e7e62f3b78b06b4364861770faaad6b0616f68
Abhijit <abhijitparida.me@gmail.com>
2016-03-20 16:35:49 +0530

    Remove caching in Db

--- src/app/abhijit/iter/Db.java
+++ src/app/abhijit/iter/Db.java
@@ -10,44 +10,21 @@ public class Db {
 
   private Context context;
   private SharedPreferences sharedPreferences;
-  private TreeSet<String> keys;
-  private String currentkey;
 
   public Db(Context context) {
     this.context = context;
     this.sharedPreferences = this.context.getSharedPreferences("bunk-v15-db", Context.MODE_PRIVATE);
-    this.keys = getkeys();
-    this.currentkey = getcurrentkey();
   }
 
   public String getCurrentKey() {
-    return this.currentkey;
+    return this.sharedPreferences.getString("currentkey", null);
   }
 
   public void setCurrentKey(String key) {
-    this.currentkey = key;
     this.sharedPreferences.edit().putString("currentkey", key).apply();
   }
 
   public String[] getAllKeys() {
-    return this.keys.toArray(new String[this.keys.size()]);
-  }
-
-  public String getValue(String key) {
-    return this.sharedPreferences.getString(key, null);
-  }
-
-  public void setValue(String key, String value) {
-    this.keys.add(key);
-    this.sharedPreferences.edit().putString(key, value).apply();
-  }
-
-  public void clearDb() {
-    this.keys.clear();
-    this.sharedPreferences.edit().clear().apply();
-  }
-
-  private TreeSet<String> getkeys() {
     TreeSet<String> keys = new TreeSet<String>();
     Map<String, ?> allentries = this.sharedPreferences.getAll();
     for (Map.Entry<String, ?> entry : allentries.entrySet()) {
@@ -55,11 +32,19 @@ public class Db {
         keys.add(entry.getKey());
       }
     }
-    return keys;
+    return keys.toArray(new String[keys.size()]);
   }
 
-  private String getcurrentkey() {
-    return this.sharedPreferences.getString("currentkey", null);
+  public String getValue(String key) {
+    return this.sharedPreferences.getString(key, null);
+  }
+
+  public void setValue(String key, String value) {
+    this.sharedPreferences.edit().putString(key, value).apply();
+  }
+
+  public void clearDb() {
+    this.sharedPreferences.edit().clear().apply();
   }
 
 }
